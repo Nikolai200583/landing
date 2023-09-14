5c29947002f7800534053a6408f597c529acbc40
Nikolai <bonum_trd@bk.ru>
2023-09-15 02:36:50 +0500

    footer_up

--- components/Footer/TheFooter.tsx
+++ components/Footer/TheFooter.tsx
@@ -26,9 +26,51 @@ const TheFooter = () => {
             </svg>
           </div>
         </div>
-        <a className="footer__up" href="#">Наверх</a>
+        <a className="footer__up" href="#">
+        <svg className="up">
+              <use xlinkHref="img/sprite.svg#up"></use>
+            </svg>
+          Наверх
+        </a>
       </div>
+      <div className="footer__section_midle">
+        <div className="section__midle_left">
+          <div className="section__midle_phone">
+            <h2 className="midle_title">Выслушаем и поможем 😉✌</h2>
+            <span className="midle__span midle_phone">
+              <svg className="phone">
+                <use xlinkHref="img/sprite.svg#phone"></use>
+              </svg>
+              +7 (952) 722-87-33
+            </span>
+            <span className="midle__span midle_mail">
+              <svg className="mail">
+                <use xlinkHref="img/sprite.svg#mail"></use>
+              </svg>
+              school@neofamily.ru
+            </span>
+            <p className="midle__text">
+              197101, г. Санкт-Петербург, вн.тер.г. муниципальный округ
+              Посадский, ул. Рентгена, д. 9Б, помещ. 1-Н, офис 9
+            </p>
+          </div>
+          <div className="section__midle_licenssion">
+            <div className="section__midle_gos">
+              <svg className="license">
+                <use xlinkHref="img/sprite.svg#license"></use>
+              </svg>
+              <p className="midle__gos_text">Государственная лицензия</p>
+            </div>
+            <div className="section__midle_skolkovo">
+              <svg className="skypro">
+                <use xlinkHref="img/sprite.svg#skypro"></use>
+              </svg>
+              <p className="midle__gos_text">Участник Skolkovo</p>
+            </div>
+          </div>
+        </div>
 
+      </div>
       <div className="footer__recuisit">
         <p>© ООО “НЕОФЭМИЛИ”</p>
         <p>ИНН: 7813660849</p>
