dfe8e92a4d64c6d6bf726f2eababfcfcf2dd21e1
Beznosov Nikolai <Bonum_trd@bk.ru>
2023-09-18 15:09:11 +0500

    onClick_spring_buttonComponent

--- components/ButtonItems/ButtonItems.tsx
+++ components/ButtonItems/ButtonItems.tsx
@@ -1,14 +1,34 @@
+'use client'
 import "./style.css";
+import React, { useState } from 'react'
+import { useSpring, animated } from '@react-spring/web'
+
 type props = {
   text: string;
   key: string;
 };
 
 const ButtonItems = ({ text }: props) => {
+
+  const [state, toggle] = useState(true)
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: state ? 1 : 0,
+    config: { duration: 1000 },
+
+  })
+
   return (
-    <a href="#" className="button__items">
+    <animated.a href="#" className="button__items" onClick={() => toggle(!state)}
+    style={{
+      opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+      scale: x.to({
+        range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+        output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+      }),
+    }}>
       {text}
-    </a>
+    </animated.a>
   );
 };
 export { ButtonItems };
